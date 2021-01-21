fb113a6e77c87721db6f7f61f16042231823c3c5
NHN <nhn@NHNui-MacBookPro.local>
2021-01-21 12:13:10 +0900

    Close #11 - Implemented list & delete from repository

--- mreview/src/test/java/org/zerock/mreview/repository/ReviewRepositoryTests.java
+++ mreview/src/test/java/org/zerock/mreview/repository/ReviewRepositoryTests.java
@@ -3,16 +3,22 @@ package org.zerock.mreview.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.zerock.mreview.entity.Member;
 import org.zerock.mreview.entity.Movie;
 import org.zerock.mreview.entity.Review;
 
+import javax.transaction.Transactional;
+import java.util.List;
 import java.util.stream.IntStream;
 
 @SpringBootTest
 public class ReviewRepositoryTests {
     @Autowired
     ReviewRepository reviewRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     public void insertReviews() {
@@ -32,5 +38,32 @@ public class ReviewRepositoryTests {
 
             reviewRepository.save(movieReview);
         });
+    }
+
+    @Test
+    public void testGetMovieReviews() {
+        Movie movie = Movie.builder().mno(2L).build();
+
+        List<Review> result = reviewRepository.findByMovie(movie);
+
+        for (Review review : result) {
+            System.out.print(review.getReviewnum());
+            System.out.print("\t" + review.getGrade());
+            System.out.print("\t" + review.getText());
+            System.out.print("\t" + review.getMember().getEmail());
+            System.out.println("-----------------------------");
+        }
+    }
+
+    @Commit
+    @Transactional
+    @Test
+    public void testDeleteMember() {
+        Long mid = 4L;
+
+        Member member = Member.builder().mid(mid).build();
+
+        reviewRepository.deleteByMember(member);
+        memberRepository.deleteById(mid);
     }
 }
